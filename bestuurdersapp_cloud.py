0f0b20979443dfd592e938eefcfe4d4351f58b42
Walida Care <kaddiri2093@gmail.com>
2026-03-09 02:16:56 +0100

    Herbouw cohortblok met retentie en nieuwe donateurs

--- bestuurdersapp_cloud.py
+++ bestuurdersapp_cloud.py
@@ -1181,55 +1181,70 @@ def render_forecast_tab(data):
         & (cohort["Datum"] <= ramadan_2025_end)
     ]
 
+    
     section_header(
         "1. Donateurbasis vergelijking",
         "Vergelijking tussen donateurs in 2025 en 2026"
     )
 
-    return_rate = (len(donors_2026) / len(donors_2025) * 100) if len(donors_2025) else 0.0
+    returned = donors_2025.intersection(donors_2026)
+    new_2026 = donors_2026 - donors_2025
 
-    c1, c2, c3, c4 = st.columns(4)
+    return_rate = (
+        len(returned) / len(donors_2025) * 100
+        if len(donors_2025)
+        else 0
+    )
+
+    c1, c2, c3, c4, c5 = st.columns(5)
 
     with c1:
         kpi_card(
             "Donateurs 2025",
             i0(len(donors_2025)),
-            "unieke donateurs met minimaal één donatie in 2025"
+            "volledige donorbasis in 2025"
         )
 
     with c2:
         kpi_card(
-            "Donateurs 2026",
-            i0(len(donors_2026)),
-            "unieke donateurs met minimaal één donatie in 2026 (t/m peildatum)"
+            "Teruggekeerd in 2026",
+            i0(len(returned)),
+            "2025-donateurs die opnieuw doneerden"
         )
 
     with c3:
         kpi_card(
             "Nog niet terug uit 2025",
             i0(len(potential)),
-            "donateurs die in 2025 doneerden maar in 2026 nog niet"
+            "resterend fondsenwervingspotentieel"
         )
 
     with c4:
         kpi_card(
-            "Terugkeerpercentage",
+            "Nieuwe donateurs 2026",
+            i0(len(new_2026)),
+            "donateurs die in 2025 niet bestonden"
+        )
+
+    with c5:
+        kpi_card(
+            "Retentiepercentage",
             f"{return_rate:.1f}%",
-            "aandeel van 2025-donateurs dat al terugkeerde in 2026"
+            "teruggekeerde donateurs / 2025-donateurs"
         )
 
     st.markdown(
         "<div class='summary'>"
         "<strong>Interpretatie:</strong><br>"
-        "Donateurs 2025 = unieke donateurs die in 2025 minimaal één donatie deden.<br>"
-        "Donateurs 2026 = unieke donateurs die in 2026 tot de peildatum hebben gedoneerd.<br>"
-        "Nog niet terug uit 2025 = donateurs uit 2025 die in 2026 nog niet zijn teruggekeerd.<br>"
-        "Terugkeerpercentage = aandeel van de 2025-donateurs dat inmiddels weer heeft gedoneerd.<br><br>"
-        "Deze laatste groep vormt het resterende fondsenwervingspotentieel."
+        "Dit blok laat zien hoe de donateurbasis van 2025 zich ontwikkelt in 2026.<br><br>"
+        "Teruggekeerd = donateurs uit 2025 die opnieuw hebben gegeven.<br>"
+        "Nog niet terug = donateurs uit 2025 die in 2026 nog niet hebben gedoneerd.<br>"
+        "Nieuwe donateurs = instroom van nieuwe donateurs in 2026.<br>"
+        "Retentiepercentage = aandeel van de 2025-donateurs dat inmiddels is teruggekeerd."
         "</div>",
         unsafe_allow_html=True
     )
-    section_header("3. Laatste 12 dagen Ramadan 2025")
+section_header("3. Laatste 12 dagen Ramadan 2025")
 
     daily12 = (
         last12
